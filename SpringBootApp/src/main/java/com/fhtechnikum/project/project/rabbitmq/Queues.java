6223fb35fcc9219cdb59e9b6a64effae007a6b2b
vermout94 <david.berger4494@gmail.com>
2024-06-22 22:49:25 +0200

    adjusted application.yml adjusted Queues names deleted db.migration in resources

--- SpringBootApp/src/main/java/com/fhtechnikum/project/project/rabbitmq/Queues.java
+++ SpringBootApp/src/main/java/com/fhtechnikum/project/project/rabbitmq/Queues.java
@@ -9,9 +9,9 @@ public enum Queues {
     //GREEN MESSAGE
     DATA_COLLECTION_DISPATCHER("dataCollectionDispatcherQueue"),
     //BLUE MESSAGE
-    STATION_DATA_COLLECTOR("deleteCharQueue"),
+    STATION_DATA_COLLECTOR("stationDataCollectorQueue"),
     //PURPLE MESSAGE
-    DATA_COLLECTION_RECEIVER("removeSpacesQueue"),
+    DATA_COLLECTION_RECEIVER("dataCollectionReceiverQueue"),
     //YELLOW MESSAGE
     PDF_GENERATOR("pdfGeneratorQueue");
 
