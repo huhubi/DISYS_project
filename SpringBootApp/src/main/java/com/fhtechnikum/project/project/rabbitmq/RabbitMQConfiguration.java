6579d436da339be0bc2970e4c1098f4d14c7de85
vermout94 <david.berger4494@gmail.com>
2024-06-23 00:09:24 +0200

    changed host and port of RabbitMQConfiguration

--- SpringBootApp/src/main/java/com/fhtechnikum/project/project/rabbitmq/RabbitMQConfiguration.java
+++ SpringBootApp/src/main/java/com/fhtechnikum/project/project/rabbitmq/RabbitMQConfiguration.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfiguration {
-    private static final String HOST = "rabbitmq";
-    private static final int PORT = 30003;
+    private static final String HOST = "localhost";
+    private static final int PORT = 5672;
 
     @Bean
     public ConnectionFactory connectionFactory() {
