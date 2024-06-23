ac056ef45b24016eb16ab1c140a454183191ab69
Gregoire <wi22b060@technikum-wien.at>
2024-06-23 20:09:17 +0200

    added JUnit tests

---
+++ PDFGenerator/src/test/java/com/fhtechnikum/project/project/PdfGeneratorTest.java
@@ -0,0 +1,67 @@
+package com.fhtechnikum.project.project;
+
+import com.fhtechnikum.project.project.model.Customer;
+import com.fhtechnikum.project.project.model.Station;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.Mockito;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+public class PdfGeneratorTest {
+
+    private PdfGenerator pdfGenerator;
+    private Customer customer;
+
+    @Before
+    public void setUp() {
+        pdfGenerator = new PdfGenerator();
+        customer = Mockito.mock(Customer.class);
+
+        when(customer.getId()).thenReturn(1);
+        when(customer.getFirstName()).thenReturn("Max");
+        when(customer.getLastName()).thenReturn("Mustermann");
+
+
+        Station s1 = new Station("Station 1", 100L, 1);
+        Station s2 = new Station("Station 2", 200L, 1);
+        Station s3 = new Station("Station 3", 300L, 1);
+
+        when(customer.getStations()).thenReturn(Arrays.asList(s1, s2, s3));
+    }
+
+    @Test
+    public void testGeneratePdf() {
+        String filePath = "../Frontend/FuelStationUI/src/main/resources/files/invoices/1.pdf";
+        
+        pdfGenerator.generatePdf(customer);
+
+        File file = new File(filePath);
+        assertTrue(file.exists());
+
+        file.delete();
+    }
+
+    @Test
+    public void generatePdfWithNoStations() throws IOException {
+        Mockito.when(customer.getStations()).thenReturn(Arrays.asList());
+
+        pdfGenerator.generatePdf(customer);
+
+        File file = new File("../Frontend/FuelStationUI/src/main/resources/files/invoices/1.pdf");
+        Assertions.assertTrue(file.exists());
+
+        String content = new String(Files.readAllBytes(Paths.get(file.getPath())));
+        Assertions.assertTrue(!content.contains("Station:"));
+    }
+
+
+}
